a8f79e8793f5e4ef20fe97e3bdcd1a343425dd35
berryhill <matthewberryhill@gmail.com>
2023-06-21 11:22:17 -0700

    Implement new data structs

---
+++ src/brc20_index/model.rs
@@ -0,0 +1,140 @@
+use mongodb::bson::{Document, DateTime, self, Bson};
+
+pub struct Ticker {
+    pub tick: str,
+    pub limit: f64,
+    pub max_supply: f64,
+    pub decimals: u8,
+    pub deploy: Deploy,
+    pub mints: Vec<Mint>,
+    pub transfers: Vec<Transfer>,
+    pub balances: HashMap<Address, UserBalance>,
+}
+
+pub struct Deploy {
+    pub max: f64,
+    pub lim: f64,
+    pub dec: u8,
+    pub block_height: u32,
+    pub tx_height: u32,
+    pub tx: GetRawTransactionResult,
+    pub inscription: Inscription,
+    pub is_valid: bool,
+}
+
+pub struct Mint {
+    pub amt: f64,
+    pub block_height: u32,
+    pub tx_height: u32,
+    pub block_time: u64,
+    pub tx: GetRawTransactionResult,
+    pub inscription: Inscription,
+    pub is_valid: bool,
+}
+
+pub struct Transfer {
+    pub amt: f64,
+    pub block_height: u32,
+    pub tx_height: u32,
+    pub tx: GetRawTransactionResult,
+    pub inscription: Inscription,
+    pub send_tx: Option<GetRawTransactionResult>,
+    pub receiver: Option<Address>,
+    pub is_valid: bool,
+}
+
+pub struct Inscription {
+    pub p: str,
+    pub op: str,
+    pub tick: str,
+    pub amt: Option<str>,
+    pub max: Option<str>,
+    pub lim: Option<str>,
+    pub dec: Option<str>,
+}
+
+pub struct Balance {
+    // todo: SAM
+}
+
+/// Mongo collections
+/// 
+
+const COLLECTION_TICKERS: str = "tickers";
+const COLLECTION_DEPLOYS: str = "deploys";
+const COLLECTION_DEPLOYS: str = "mints";
+const COLLECTION_DEPLOYS: str = "transfers";
+const COLLECTION_INSCRIPTIONS: str = "inscriptions";
+const COLLECTION_TRANSACTIONS: str = "transactions";
+
+pub struct TickerDB {
+    pub id: bson::oid,
+    pub tick: str,
+    pub limit: f64,
+    pub max_supply: f64,
+    pub decimals: u8,
+    pub deploy: bson::oid,
+    pub mints: Vec<bson::oid>,
+    pub transfers: Vec<bson::oid>,
+    pub created_at: DateTime,
+}
+
+pub struct DeployDB {
+    pub id: bson::oid,
+    pub max: f64,
+    pub lim: f64,
+    pub dec: u8,
+    pub block_height: u32,
+    pub tx_height: u32,
+    pub tx: GetRawTransactionResult,
+    pub inscription: Inscription,
+    pub is_valid: bool,
+    pub created_at: DateTime,
+}
+
+pub struct MintDB {
+    pub id: bson::oid,
+    pub amt: f64,
+    pub block_height: u32,
+    pub tx_height: u32,
+    pub block_time: u64,
+    pub tx: GetRawTransactionResult,
+    pub inscription: Inscription,
+    pub is_valid: bool,
+    pub created_at: DateTime,
+}
+
+pub struct TransferDB {
+    pub id: bson::oid,
+    pub amt: f64,
+    pub block_height: u32,
+    pub tx_height: u32,
+    pub tx: GetRawTransactionResult,
+    pub inscription: Inscription,
+    pub send_tx: Option<GetRawTransactionResult>,
+    pub receiver: Option<Address>,
+    pub is_valid: bool,
+    pub created_at: DateTime,
+}
+
+pub struct InscriptionDB {
+    pub id: bson::oid,
+    pub p: str,
+    pub op: str,
+    pub tick: str,
+    pub amt: Option<str>,
+    pub max: Option<str>,
+    pub lim: Option<str>,
+    pub dec: Option<str>,
+    pub created_at: DateTime,
+}
+
+pub struct TransactionDB {
+    pub id: bson::oid,
+    pub tx: GetRawTransactionResult,
+    pub created_at: DateTime,
+}
+
+pub struct Balance {
+    // todo: SAM
+}
